8b94ba617ba1f94ec966191f0bd590197063fec2
Hysgddyhd <129603595+Hysgddyhd@users.noreply.github.com>
2024-03-30 22:31:36 +0800

    Add files via upload

---
+++ readFile.py
@@ -0,0 +1,13 @@
+myFile=open("readme.md",'r')
+
+a=myFile.readline()
+b=myFile.readline()
+c=myFile.readline()
+d=myFile.readline()
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+myFile.close()
---
+++ readme.md
@@ -0,0 +1,4 @@
+My name : Wu yuntian
+Faculty : FTSM
+Course : Computer Science
+Track : Software Technology
